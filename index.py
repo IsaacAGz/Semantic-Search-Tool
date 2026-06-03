db44833afaad7386685329d7d8ad49b5a8b9b333
Isaac Angulo <isaac.angulogomez@gmail.com>
2026-06-03 11:55:19 -0700

    Fix: Rename main.py to index.py and update vercel configuration

---
+++ index.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import os
+import google.generativeai as genai
+from dotenv import load_model, load_dotenv
+
+app = FastAPI()
+load_dotenv()
+
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+
+class QueryRequest(BaseModel):
+    context: str
+    question: str
+
+    @app.post("/api/ask")
+    async def ask_demini(payload: QueryRequest):
+        try:
+            model = genai.GenerativeModel('gemini-1.5-flash')
+
+            prompt = f"""
+            You are a lightweight AI assitant. Use the following context to answer the the user's questions concisely.
+
+            Context: {payload.context}
+            Question: {payload.question}
+            Asnwer:
+            """
+
+            response = model.generative_content(prompt)
+            return {"answer": response.text}
+        
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
